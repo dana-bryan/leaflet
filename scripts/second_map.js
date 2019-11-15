1353b82501af7d54c863d88944dc9f9aeda9be45
dana-bryan <dana-bryan@users.noreply.github.com>
2019-11-15 16:39:08 -0500

    Final Version

--- scripts/second_map.js
+++ scripts/second_map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('floridattractions').setView([27.360351, -80.149896], 5.83);
+var mymap = L.map('floridattractions').setView([27.360351, -80.149896], 6.3);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/danabryan/ck2xhan9a0jws1cnzrcgzrpnv/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGFuYWJyeWFuIiwiYSI6ImNrMnhncjBjdDBiYjYzanJzNjEyYWNxeXMifQ.7zaDEQ3kz0MDRkds2wBVXQ', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
